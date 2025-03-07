ade0c4b64443f1cb9606642eb84a559f806da7f4
EvolveAniolku <ajhoyoschicho0111@gmail.com>
2025-03-07 17:08:30 -0500

    inicio del codigo

--- Proyectos/Ruleta.py
+++ Proyectos/Ruleta.py
@@ -1,3 +1,14 @@
-## Ruleta de Casino
-## Autor: Juan Pablo hoyos chichoque
-print("hello world")
+"""
+Ruleta de Casino 
+
+INTEGRANTES 
+- Juan Pablo Hoyos Chichoque
+- Ricardo Farid Cuero Ruiz
+- Brayan David Garces Quintero
+"""
+import random
+## Libreria para el uso de numeros aleatorios
+
+Rango_de_numeros=list(range(0,37))  
+
+
