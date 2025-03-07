7390d49a77e93d4234bf6243f8948cd355828c56
Fahrid69 <ricueroruiz@outlook.com>
2025-03-07 18:06:50 -0500

    avance

--- Proyecto-Ruleta/Ruleta.py
+++ Proyecto-Ruleta/Ruleta.py
@@ -1,5 +1,11 @@
-## Ruleta de Casino
-## Autor: Juan Pablo hoyos chichoque
-print("hello world")
+# INTENTO DE SIMULACION (RULETA DE CASINO)
+import random
 
-# farid modifico esto
+def giro_ruleta ()
+    giro = random.radint(0,37)
+    return (giro)
+
+def juego ()
+print('Bienvenido a la Simulacion de una Ruleta')
+numero_apostado = input(int('Por favor, ingrese el numero por el cual desea apostar: '))
+
